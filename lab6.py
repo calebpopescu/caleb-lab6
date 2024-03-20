6b6087e734857468c67968beb13fe1d1c2051473
sparrell1 <163921853+sparrell1@users.noreply.github.com>
2024-03-20 15:52:43 -0400

    Update lab6.py
    added decode option

--- lab6.py
+++ lab6.py
@@ -7,6 +7,13 @@ def encode_password(password):
         encoded_password += shifted_digit
     return encoded_password
 
+def decode_password(encoded_password):
+    decoded_password = ""
+    for digit in encoded_password:
+        shifted_digit = str((int(digit) - 3) % 10)  # shift each digit down 3 numbers
+        decoded_password += shifted_digit
+    return decoded_password
+
 # main program loop
 while True:
     print("Menu")
@@ -20,6 +27,10 @@ while True:
         password = input("Please enter your password to encode: ")
         encoded_password = encode_password(password)
         print("Your password has been encoded and stored!")
+    elif choice == "2":
+        encoded_password = input("Please enter your encoded password to decode: ")
+        decoded_password = decode_password(encoded_password)
+        print("The encoded password is {}, and the original password is {}.".format(encoded_password, decoded_password))
     elif choice == "3":
         break
     else:
